3a97eb9c918c949ca62d64b66d6e6b02888a8818
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2013-02-20 00:52:05 +0200

    Preparing for many instruments in bass.c.

--- bass.c
+++ bass.c
@@ -7,19 +7,34 @@
 #define TIME .42f
 #define STIME .07f
 
+typedef struct {
+	int sfreq;
+	int slowdown;
+	int volume;
+	int stime;
+} instr;
+instr instrs[] =  {
+	{105000, 12, 180, FREQ*0.07},
+};
+
 int place=0;
 void callback(void* udata, Uint8* s, int len)
 {
 	(void)udata,(void)len;
 	Uint16* stream = (Uint16*)s; len/=2;
 
+	instr ins = instrs[0];
+	const int sfreq = ins.sfreq, slowdown = ins.slowdown, volume = ins.volume, stime = ins.stime;
+
+	const int limit = FREQ*TIME - stime, silence = (1<<20)/stime;
+
 	int i;
- 	const int sfreq = 1050;
+// 	const int sfreq = 1050;
 // 	const float sfreq = .0805f;
-	const int slowdown = 12;
+//	const int slowdown = 12;
 //	const float slowdown = .000512f;
-	const int limit = FREQ*(TIME-STIME);
-	const int silence = (1<<20)/(FREQ*STIME);
+//	const int limit = FREQ*(TIME-STIME);
+//	const int silence = (1<<20)/(FREQ*STIME);
 	//	printf("starting freq: %f\n", sfreq-place*slowdown);
 	for(i=0; i<len; ++i) {
 		int k = place+i;
@@ -27,7 +42,7 @@ void callback(void* udata, Uint8* s, int len)
 		else stream[i] = 127.9*sin((sfreq-k*slowdown)*k);*/
 //		float a = 20000*sin(sfreq*k/(10000+k*slowdown));
 //		float a = 20000*fmod(sfreq*k/(10000+k*slowdown),1);
-		int a = ((int)(100*sfreq*k/(10000+k*slowdown)) & 127) * 180;
+		int a = ((int)(sfreq*k/(10000+k*slowdown)) & 127) * volume;
 		if (k > limit) {
 //			stream[i] /= 1+silence*(k-limit);
 			a *= ((1<<20)-silence*(k-limit))>>20;
