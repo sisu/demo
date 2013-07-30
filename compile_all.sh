0a2809261bf5dff95b8eafd5505ceae4c94d939a
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2013-07-31 01:02:01 +0300

    Added compile script for producing different resolution versions.

---
+++ compile_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf out
+mkdir -p out
+for t in fullscreen window; do
+	for r in "800 600" "1024 768" "1280 720"; do
+		read x y <<< $r
+		./cshader WIDTH $x HEIGHT $y < march.frag > t.frag.small
+		if [ $t == fullscreen ]; then f=0x80000000; else f=0; fi
+		nasm -f bin intro.asm -o t -dWIDTH=$x -dHEIGHT=$y -dFULLSCREEN=$f
+		7z a -tGZip -mx=9 t.gz t
+		p="out/intro_${x}x${y}_${t}"
+		cat unpack.header t.gz > $p
+		rm t t.gz
+		chmod a+rx $p
+	done
+done
+ls -l out
